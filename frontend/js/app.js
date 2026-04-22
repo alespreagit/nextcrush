2aed72220edfc2716a21eedc937d5f3078f62719
alespreagit <66039914+alespreagit@users.noreply.github.com>
2026-04-22 02:34:30 +0000

    fix: remove duplicate API_BASE

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,3 @@
-const API_BASE = "https://3vpmm532vwxmruchj6dsz6xfhu0sagil.lambda-url.us-east-1.on.aws";
 // ═══════════════════════════════════════════
 // APP.JS — Main application logic
 // ═══════════════════════════════════════════
